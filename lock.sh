7cba1be5a89ff57115dcb5e65843182c524522d8
Theo Massard <massar_t@etna-alternance.net>
2017-11-22 15:22:27 +0000

    Add scripts gitmail, lock.sh

---
+++ lock.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#scrot /tmp/screenshot.png
+#i3lock
+#convert /tmp/screenshot.png -blur 0x5 /tmp/screenshot.png
+#convert /tmp/screenshot.png -scale 10% -scale 1000% /tmp/screenshot.png
+#pkill i3lock && i3lock -i /tmp/screenshot.png
+
+IMG=/usr/local/share/images/vap_big.png
+
+B='#00000000'  # blank
+C='#ffffff22'  # clear ish
+D='#ff00ffcc'  # default
+T='#ee00eeee'  # text
+W='#880000bb'  # wrong
+V='#bb00bbbb'  # verifying
+
+i3lock                \
+--image $IMG          \
+--insidevercolor=$C   \
+--ringvercolor=$V     \
+\
+--insidewrongcolor=$C \
+--ringwrongcolor=$W   \
+\
+--insidecolor=$B      \
+--ringcolor=$D        \
+--linecolor=$B        \
+--separatorcolor=$D   \
+\
+--textcolor=$T        \
+--keyhlcolor=$W       \
+--bshlcolor=$W        \
+\
+--screen 0            \
+--indicator			  \
+--clock               \
+--timestr="%H:%M:%S"  \
+--datestr="%A, %m %Y" \
+--blur 1              \
+\
